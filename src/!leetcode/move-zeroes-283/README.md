beeba0a9c08e67773c49229f5270dfadbdf475aa
Daniil Polovinkin <dolovinkin@gmail.com>
2022-07-12 15:52:25 +0400

    chore: updated task statuses

--- src/!leetcode/move-zeroes-283/README.md
+++ src/!leetcode/move-zeroes-283/README.md
@@ -1,4 +1,4 @@
-Task status
+Task Status
 ===========
 Solved ✅
 
--- src/!leetcode/move-zeroes-283/v1.js
+++ src/!leetcode/move-zeroes-283/v1.js
@@ -1,20 +1,20 @@
 /**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
- var moveZeroes = function(nums) {
+* @param {number[]} nums
+* @return {void} Do not return anything, modify nums in-place instead.
+*/
+var moveZeroes = function(nums) {
   const zeroIdc = [];
   for(let i = 0; i < nums.length; i++) {
-      if(nums[i] === 0) {
-          zeroIdc.push(i);
-      }
+    if(nums[i] === 0) {
+      zeroIdc.push(i);
+    }
   }
   console.log({nums, zeroIdc});
   for(let j = 0; j < zeroIdc.length; j++) {
-      const zeroIdx = zeroIdc[j] - j;
-      nums.splice(zeroIdx, 1);
-      console.log({nums, zeroIdx});
-      nums.push(0);
+    const zeroIdx = zeroIdc[j] - j;
+    nums.splice(zeroIdx, 1);
+    console.log({nums, zeroIdx});
+    nums.push(0);
   }
   return nums;
 };
