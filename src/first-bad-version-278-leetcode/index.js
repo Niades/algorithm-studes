f69c295317e39910c5966c781bd41d743a7fa9e5
Daniil Polovinkin <dolovinkin@gmail.com>
2022-07-12 15:19:17 +0400

    WIP: feat(first-bad-version-278-leetcode): remove array at all

--- src/first-bad-version-278-leetcode/index.js
+++ src/first-bad-version-278-leetcode/index.js
@@ -3,57 +3,65 @@
 * 
 * @param {integer} version number
 * @return {boolean} whether the version is bad
-* isBadVersion = function(version) {
-*     ...
-* };
 */
-
-/**
-* @param {function} isBadVersion()
-* @return {function}
-*/
-var solution = function(isBadVersion) {
-  /**
-  * @param {integer} n Total versions
-  * @return {integer} The first bad version
-  */
-  return function(n) {
-    const hayStack = Array.from(Array(n), (_, i) => i + 1);
-    return binarySearchMod(n, hayStack, isBadVersion)
-  };
+const isBadVersion = function(version) {
+  if(version >= 4) {
+    return true;
+  }
+  return false;
 };
+  
+  /**
+  * @param {function} isBadVersion()
+  * @return {function}
+  */
+  var solution = function(isBadVersion) {
+    /**
+    * @param {integer} n Total versions
+    * @return {integer} The first bad version
+    */
+    return function(n) {
+      return binarySearchMod(n, isBadVersion)
+    };
+  };
+  
+  function binarySearchMod(n, isBadVersion) {
+    if(n === 1) {
+      return n;
+    }
+    let left = 0, right = n - 1;
+    const totalLength = right - left;
+    while(left < right) {
+      console.log("* Iteration starting, left = ", left, ", right = ", right);
+      const length = right - left;
+      const middleIdx = Math.floor(length / 2);
+      const middleValue = left + middleIdx + 1;
+      const middleValueBad = isBadVersion(middleValue);
+      console.log({ length, middleIdx, middleValue, middleValueBad });
+      if(length === 0) {
+        return -1;
+      }
+      if(middleValueBad) {
+        console.log("before isBadVersion", isBadVersion(middleValue - 1))
+        if(length >= 2 && isBadVersion(middleValue - 1)) {
+          console.log("Found bad version, but continuing.")
+        } else {
+          return left - 1;
+        }
+      }
+      if (middleValue > n) {
+        console.log("Going right")
+        left = totalLength - middleIdx + 1;
+        right = 
+        console.log("* Iteration end ", {left, right});
+      } else if (middleValue < n) {
+        console.log("Going left")
+        left = Math.max(totalLength - length - middleIdx + 1, 0);
+        right = totalLength - middleIdx;
+        console.log("* Iteration end ", {left, right});
+      }
+    }
+    return left;
+  }
 
-function binarySearchMod(n, haystack, isBadVersion) {
-  if(haystack.length === 0) {
-    return -1;
-  }
-  if(n === 1) {
-    return n;
-  }
-  const middleIdx = Math.floor(haystack.length / 2);
-  const middleValue = haystack[middleIdx];
-  const middleValueBad = isBadVersion(middleValue);
-  console.log({ middleIdx, middleValue, middleValueBad });
-  if(isBadVersion(middleValue)) {
-    console.log("before isBadVersion", isBadVersion(middleValue - 1))
-    if(haystack.length >= 2 && isBadVersion(middleValue - 1)) {
-      console.log("Found bad version, but continuing.")
-    } else {
-      return middleValue;
-    }
-  }
-  if (middleValueBad) {
-    console.log("Going left")
-    const leftHaystack =  haystack.slice(
-      0, 
-      haystack.length - middleIdx
-      )
-      //console.log({leftHaystack})
-      return binarySearchMod(n, leftHaystack, isBadVersion, n);
-    } else if (!middleValueBad) {
-      console.log("Going right")
-      const rightHaystack = haystack.slice(middleIdx + 1, haystack.length)
-      //console.log({rightHaystack})
-      return binarySearchMod(n, rightHaystack, isBadVersion)
-    }
-  }
+  console.log("result = ", solution(isBadVersion)(5));
